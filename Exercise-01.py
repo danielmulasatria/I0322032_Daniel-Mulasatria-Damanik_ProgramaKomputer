694f71a9ffeb11bee7725d81375713447ebee815
danielmulasatria <danielmulasatria@student.uns.ac.id>
2023-03-12 17:16:54 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,7 @@
+print("Daniel Mulasatria Damanik")
+print("I0322032\n")
+
+print("Perkenalkan nama saya Daniel Mulasatria Damanik dengan NIM I0322032. Saya lahir di Medan, namun saya tumbuh dan besar di Pontianak, Kalimantan Barat. Pontianak dikenal dengan Kota Khatulistiwa karena kota ini dilalui oleh garis khatulistiwa. Pontianak memiliki cuaca yang cukup panas, bahkan mungkin lebih panas dari Kota Solo. Hal ini terjadi kemungkinan karena letaknya berada di garis khatulistiwa. Kota Pontianak juga berbatasan langsung dengan Serawak, Malaysia.\n")
+print("Sebagai Kota Khatulistiwa, Pontianak memiliki Tugu Khatulistiwa dimana tugu tersebut menjadi tepat lewatnya garis yang membelah bumi bagian selatan dan bagian utara. Namun, di tempat saya tinggal jaraknya lumayan jauh dari titik khatulistiwa sehingga bisa memakan waktu hingga 30 menit untuk berkunjung ke tugu tersebut. Biaya hidup di kota saya apabila dibandingkan dengan Solo mungkin bisa dibilang cukup lebih mahal, namun biaya tersebut termasuk standar apabila dibandingkan dengan kota-kota lain. \n")
+
+print("Sebagai mahasiswa Teknik Industri saya berharap ketika lulus nanti saya bisa memanfaatkan dan serta menerapkan ilmu-ilmu yang sudah dipelajari ke dalam masyarakat sehingga ilmu tersebut bisa membantu untuk menyejahterakan kehidupan masyarakat di Indonesia. Selain itu, saya juga berharap nantinya bisa membahagiakan diri sendiri dan kedua orang tua.")
